689cf3b3723511c0164e6b4b482378687131f5cc
daneel <ddmng@aa.com>
2016-02-23 17:23:40 +0100

    Added web content

--- src/main/java/it/danielediminica/Runner.java
+++ src/main/java/it/danielediminica/Runner.java
@@ -2,19 +2,48 @@ package it.danielediminica;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.function.Consumer;
 
 public class Runner extends AbstractVerticle {
+    static Router router;
+    static Vertx vertx;
+    private final static Logger logger = io.vertx.core.logging.LoggerFactory.getLogger(Runner.class.getName());
 
-  @Override
-  public void start() throws Exception {
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!"))
-    .listen(
-        Integer.getInteger("http.port"), System.getProperty("http.address"));
-  }
+    @Override
+    public void start() throws Exception {
 
-  public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new Runner());
-  }
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(Integer.getInteger("http.port"), System.getProperty("http.address"));
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Consumer<Vertx> runner = vertx -> {
+            router = Router.router(vertx);
+            vertx.deployVerticle(new Runner());
+
+            router.route("/now").handler(h -> {
+                h.response().sendFile("webroot/now.html");
+            });
+
+            logger.debug("Adding static handler");
+            router.route("/*").handler(StaticHandler.create().setCachingEnabled(false));
+
+        };
+
+        vertx = Vertx.vertx();
+        runner.accept(vertx);
+
+        logger.info("Server started");
+
+    }
 
 }
